e876b590d064a7bf4329c51a6b4d63a7812a5cf9
Gila Shouraky <gilasho1234@gmail.com>
2026-02-26 00:49:56 +0200

    Update smoove.js

--- netlify/functions/smoove.js
+++ netlify/functions/smoove.js
@@ -6,13 +6,13 @@ exports.handler = async (event) => {
   try {
     const data = JSON.parse(event.body);
     const SMOOVE_API_KEY = '2b930959-167d-45ec-989d-29b63173fc50';
+    const SMOOVE_LIST_ID = 1117962;
     const headers = {
       'Content-Type': 'application/json',
       'Authorization': 'Bearer ' + SMOOVE_API_KEY,
     };
 
-    // Create/update contact with tag
-    const createRes = await fetch('https://rest.smoove.io/v1/Contacts?updateIfExists=true', {
+    const res = await fetch('https://rest.smoove.io/v1/Contacts?updateIfExists=true', {
       method: 'POST',
       headers,
       body: JSON.stringify({
@@ -20,11 +20,12 @@ exports.handler = async (event) => {
         lastName: data.lastName,
         email: data.email,
         cellPhone: data.cellPhone,
-        tags: ['רכישה-אתר'],
+        campaignSource: 'website-purchase',
       }),
     });
-    const createText = await createRes.text();
-    console.log('Create with tag:', createRes.status, createText.substring(0, 300));
+    const resText = await res.text();
+    const resData = JSON.parse(resText);
+    console.log('Contact:', resData.id, 'campaignSource:', resData.campaignSource);
 
     return { statusCode: 200, body: JSON.stringify({ success: true }) };
   } catch (err) {
