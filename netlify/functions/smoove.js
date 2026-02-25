371c5971081e3ec0bae22cd1646254cf6a8bee89
Gila Shouraky <gilasho1234@gmail.com>
2026-02-25 20:35:18 +0200

    ד

--- netlify/functions/smoove.js
+++ netlify/functions/smoove.js
@@ -5,28 +5,46 @@ exports.handler = async (event) => {
 
   try {
     const data = JSON.parse(event.body);
+    const SMOOVE_API_KEY = '50f2b9e9-534f-49d5-8dc4-2b05ec90039c';
+    const SMOOVE_LIST_ID = 1117962;
+    const headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + SMOOVE_API_KEY,
+    };
 
-    const response = await fetch('https://rest.smoove.io/v1/Contacts?updateIfExists=true&listId=' + 1117962, {
+    // Step 1: Create or update contact
+    const contactRes = await fetch('https://rest.smoove.io/v1/Contacts?updateIfExists=true', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 50f2b9e9-534f-49d5-8dc4-2b05ec90039c',
-      },
+      headers,
       body: JSON.stringify({
         firstName: data.firstName,
         lastName: data.lastName,
         email: data.email,
         cellPhone: data.cellPhone,
-        lists_Linked: [1117962],
       }),
     });
 
-    const text = await response.text();
-    console.log('Smoove response:', response.status, text);
+    const contactData = await contactRes.json();
+    console.log('Contact upsert:', contactRes.status, JSON.stringify(contactData));
+
+    const contactId = contactData.id;
+    if (!contactId) {
+      return { statusCode: 500, body: 'No contact ID returned' };
+    }
+
+    // Step 2: Add contact to list
+    const listRes = await fetch('https://rest.smoove.io/v1/Lists/' + SMOOVE_LIST_ID + '/Contacts', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify([contactId]),
+    });
+
+    const listText = await listRes.text();
+    console.log('Add to list:', listRes.status, listText);
 
     return {
-      statusCode: response.ok ? 200 : response.status,
-      body: text,
+      statusCode: 200,
+      body: JSON.stringify({ contact: contactId, list: listRes.status }),
     };
   } catch (err) {
     console.error('Smoove error:', err);
